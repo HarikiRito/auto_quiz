7a9067a90e9cb6cef5b263003988dd2e871033ce
hariki <nxh0809@gmail.com>
2019-08-04 21:43:32 +0700

    OpenCV

--- gui/main.py
+++ gui/main.py
@@ -1,13 +1,34 @@
 from tkinter import *
 from tkinter import messagebox
+import numpy as np
+import pyautogui
+import imutils
+import cv2
 
-top = Tk()
+from gui.input_grid import BuilderUI
+
+app = Tk()
+
+app.geometry('500x500')
+buider = BuilderUI(app)
+
+buider.input_cor()
 
 
 def helloCallBack():
-    messagebox.showinfo("Hello Python", "Hello World")
+    a = [376, 135]
+    b = [590, 368]
+    c = list(np.subtract(b, a))
+    region = a + c
+    image = pyautogui.screenshot(region=region)
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    cv2.imwrite("test.png", image)
 
 
-B = Button(top, text="Hello", command=helloCallBack)
-C = Button(top, text="Hello 2", command=helloCallBack)
-top.mainloop()
+e1 = Entry(app)
+e2 = Entry(app)
+
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
+
+app.mainloop()
