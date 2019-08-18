be8eeb74c76c97c4af8ed50fda3f93facadbdb68
hariki <nxh0809@gmail.com>
2019-08-18 23:17:14 +0700

    OpenCV + Tkinter

--- api/main_api.py
+++ api/main_api.py
@@ -1,26 +1,63 @@
-import os
-import time
-from random import random, randint
-from flask import Flask, request
-from flask_restful import Resource, Api
-from json import dumps
-from flask_jsonpify import jsonify
+from flask import request, url_for
+from flask_api import FlaskAPI, status, exceptions
+from flask_cors import CORS
 
-from api.utils import api
-from text_extract.clould_vision import detect_text
+from api.seach import google_search_get
 
-app = Flask(__name__)
-app_api = Api(app)
+app = FlaskAPI(__name__)
+CORS(app)
 
-
-class Employees(Resource):
-    @api.beauty
-    def get(self):
-        a = 'Kẻ có'
-        return {'a': a}
+notes = {
+    0: 'do the shopping',
+    1: 'build the codez',
+    2: 'paint the door',
+}
 
 
-app_api.add_resource(Employees, '/test')  # Route_1
+def note_repr(key):
+    return {
+        'url': request.host_url.rstrip('/') + url_for('notes_detail', key=key),
+        'text': notes[key]
+    }
 
-if __name__ == '__main__':
+
+@app.route("/search", methods=['GET', 'POST'])
+def notes_list():
+    """
+    List or create notes.
+    """
+    if request.method == 'POST':
+        q: str = request.data.get('q')
+        a: str = request.data.get('a')
+        result = google_search_get(q, a)
+        return {
+            'count': result,
+            'count_appear': 'c'
+        }
+
+    # request.method == 'GET'
+    return {'count': 'b', 'count_appear': 'c'}
+
+
+@app.route("/<int:key>/", methods=['GET', 'PUT', 'DELETE'])
+def notes_detail(key):
+    """
+    Retrieve, update or delete note instances.
+    """
+    if request.method == 'PUT':
+        note = str(request.data.get('text', ''))
+        notes[key] = note
+        return note_repr(key)
+
+    elif request.method == 'DELETE':
+        notes.pop(key, None)
+        return '', status.HTTP_204_NO_CONTENT
+
+    # request.method == 'GET'
+    if key not in notes:
+        raise exceptions.NotFound()
+    return note_repr(key)
+
+
+if __name__ == "__main__":
     app.run(port=8888, debug=True, passthrough_errors=True)
--- api/seach.py
+++ api/seach.py
@@ -4,7 +4,7 @@ from typing import List, Tuple, Coroutine
 from text_extract.pil_utils import GoogleSearch
 
 
-async def google_search_get(q: str, a: str) -> Tuple[int, int]:
+def google_search_get(q: str, a: str) -> Tuple[int, int]:
     search_instance = GoogleSearch('{} - {}'.format(q, a))
     search_instance.search()
     results = search_instance.result
@@ -17,18 +17,6 @@ async def google_search_get(q: str, a: str) -> Tuple[int, int]:
     return total, count_appear
 
 
-async def search_all(q: str, answers: List[str]):
-    return await asyncio.wait([
-        google_search_get(q, a) for a in answers
-    ])
-
-
 a = 'Vi công chúa Việt Nam nào sau dây có băng thac sĩ nuóc ngoài?'
 b = ['Nguyen Phúc Nhu Lý', 'Nguyěn Phúc Phuong Dung', 'Nguyen Phúc Nhu Mai']
-
-# r = asyncio.run(
-#     search_all(a, b)
-# )
 #
-# print(r)
-
